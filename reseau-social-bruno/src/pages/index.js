a496dd12d643f9ac988b9224ba43410fe2c28ee1
Bruno Mahiet <devwebmb@gmail.com>
2023-03-26 20:46:58 +0200

    test

--- reseau-social-bruno/src/pages/index.js
+++ reseau-social-bruno/src/pages/index.js
@@ -6,11 +6,15 @@ import Login from "../components/connect/Login";
 import Signup from "../components/connect/Signup";
 
 export default function Home() {
-  useEffect(() => {
-    axios
-      .get(`http://127.0.0.1:3333/user/13`)
-      .then((data) => console.log(data));
-  });
+  // useEffect(() => {
+  //   axios
+  //     .post(`http://127.0.0.1:3333/user/create`, {
+  //       username: "TestCreate",
+  //       email: "test_create@test.com",
+  //       password: "test",
+  //     })
+  //     .then((data) => console.log(data));
+  // });
   return (
     <div className="home d-flex align-items-center flex-column">
       <div className="col-lg-3 col-md-6 col-8 mx-auto d-flex justify-content-center my-5 ">
